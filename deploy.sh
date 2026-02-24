2a5e8522652445544dc6382e63d0f09d13c7aa2d
Hang Yeung <yeungh@umich.edu>
2026-02-24 02:59:21 -0500

    Add up/down and yaml discovery to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-docker compose -f docker/caddy.yml \
-               -f docker/transition-site.yml up -d --pull always
+set -euo pipefail
+
+# Usage check
+if [[ $# -ne 1 ]] || [[ "$1" != "up" && "$1" != "down" ]]; then
+    echo "Usage: $0 [up|down]"
+    exit 1
+fi
+
+ACTION=$1
+
+# Run docker compose
+compose_files=$(find docker -type f -name "*.yml" | sort | sed 's/^/-f /')
+
+echo "Running 'docker compose $ACTION' with files:"
+echo "$compose_files"
+
+if [[ "$ACTION" == "up" ]]; then
+    docker compose $compose_files up -d --pull always
+else
+    docker compose $compose_files down
+fi
